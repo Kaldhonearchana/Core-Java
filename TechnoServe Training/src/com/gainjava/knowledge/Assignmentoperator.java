577e37f9b08e29a6369e4be58b5efbf51588cce5
kaldh <kaldh@LAPTOP-GVS7KU40>
2022-03-19 15:53:44 +0530

    First commit

---
+++ TechnoServe Training/src/com/gainjava/knowledge/Assignmentoperator.java
@@ -0,0 +1,15 @@
+package com.gainjava.knowledge;
+
+public class Assignmentoperator {
+	public static void main(String[] args)
+	{
+		int x = 4;
+		x += 5;  
+        System.out.println(x);
+		x -= 5;		
+        x *= 5; 
+		x /= 4;
+		x %= 5;
+		System.out.println(x);
+	}
+}
